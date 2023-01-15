9025e2e18afa6734ac95b79caca7b5466293492b
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-01-15 09:12:53 -0800

    Added the winning function, play again functionality, and Winner display at the top.

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const SHAPES = {
     '1' : 'X',
     '-1' : 'O'
 }
-
+const WINSTATES = [
+    [[0,0],[0,1],[0,2]], /// First Row Horizontal Win
+    [[0,0],[1,0],[2,0]], /// First Column Vertical Win
+    [[1,0],[1,1],[1,2]], /// Second Row Horizontal Win 
+    [[0,1],[1,1],[2,1]], /// Second Column Vertical Win
+    [[2,0],[2,1],[2,2]], /// Third Row Howizontal Win
+    [[0,2],[1,2],[2,2]], /// Third Column Vertical Win
+    [[0,0],[1,1],[2,2]], /// NWSE Diag Win
+    [[0,2],[1,1],[2,0]] /// NESW Diag Win
+];
 
 /*----- state variables -----*/
 let board;
@@ -17,7 +26,9 @@ const zones = document.querySelectorAll(".TTT-Zone")
 /*----- event listeners -----*/
 zones.forEach(function(zone) {
     zone.addEventListener("click", function() {
-        
+        if (winner !== null) {
+            return
+        } else {
         let coordfinder = event.target.id.split("");
         let boardCol = parseInt([coordfinder[5]]);
         let boardRow = parseInt([coordfinder[7]]);
@@ -28,7 +39,10 @@ zones.forEach(function(zone) {
         }
         render();
         changeTurn();
-    })
+    }})
+})
+playAgainBtn.addEventListener("click", function() {
+ init();
 })
 /*----- functions -----*/
 init();
@@ -43,6 +57,7 @@ winner = null;
 render();
 }
 function render() {
+    renderPlayAgain();
     renderBoard();
     renderMessage();
 }
@@ -58,12 +73,54 @@ function renderBoard() {
         });
     });
 }
-
-function renderMessage() {
+function renderMessage(){
+    findWinner();
+    if(winner !== null) {
+    return messageEl.innerHTML = `${SHAPES[winner]} Wins!`;
+} else if (winner === "T") {
+    return messageEl.innerHTML = "It's a tie???"
+} else {
     return messageEl.innerHTML = `${SHAPES[turn]}'s Turn!`;
+}
+
+}
+function renderPlayAgain() {
+    if(winner === null) {
+        playAgainBtn.style.visibility = "hidden";
+        playAgainBtn.style.opacity = "0";
+    }  else {
+        playAgainBtn.style.visibility = "visible";
+        playAgainBtn.style.opacity = "1";
+    }
 }
 function changeTurn() {
     turn = turn * -1;
     renderMessage();
+    renderPlayAgain();
 }
+function findWinner() {
+    for (let i =  0; i < WINSTATES.length; i++) {
+        let winstate = WINSTATES[i];
+        let a = board[winstate[0][0]][winstate[0][1]];
+        let b = board[winstate[1][0]][winstate[1][1]];
+        let c = board[winstate[2][0]][winstate[2][1]];
+        if (a === b && b === c && a !== 0) {
+            winner = a;
+            break;
+        }
+    }
+    if (!winner) {
+        let isDraw = true;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] === 0) {
+                    isDraw = false;
+                    break;
+                }
+            }
+        }
+        if (isDraw) winner = "T";
+    }
+}
+
 
